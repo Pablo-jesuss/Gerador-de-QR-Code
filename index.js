aa459b5dfeeb3ed3b94f17b817ae163e08dccf96
Pablo-jesuss <pablo.programador26@gmail.com>
2023-10-14 21:42:57 -0300

    commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function generaterQrCode() {
 
   qrCodeBtn.innerText = "Gerando código...";
 
-  qrCodeImg = src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue} `;
+  qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue} `;
 
   container.classList.add("active");
 }
