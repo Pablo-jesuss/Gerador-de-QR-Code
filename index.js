13301731d52d33c1a32a5072adc9333e03c95e11
Pablo-jesuss <pablo.programador26@gmail.com>
2023-10-14 21:13:50 -0300

    const

---
+++ index.js
@@ -0,0 +1,8 @@
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+
+// Eventos ---
+
+qrCodeBtn.addEventListener("click", () => {
+  
+})
